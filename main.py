c78616efdc735b0d3c19b1e40f06266c58c23c27
Chayka <58815424+kamome-inc@users.noreply.github.com>
2020-02-03 13:09:21 +0300

    Do Class LoopOver
    right and left - done
    string to list of lists - done

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
-input_ = "ABCDEFGHI".replace('\n', '')
-s = 0
-m = []
-
-for i in range(3):
-    a = []
-    for j in range(3):
-        a.append(input_[s])
-        s+=1
-    m.append(a)
+input_A = "ACS\nBDS\nDCS"
+input_B = "AB\nCD"
 
 
+class LoopOver:
+
+    def __init__(self, string1, string2):
+        # In list value i save all lines of input as lists with each elements of line
+        # In variable size are exist size of matrix
+        self.solution = []
+        self.mixedUpBoard = []
+        for line in string1.split('\n'):
+            arr = []
+            for sign in line:
+                arr.append(sign)
+            self.mixedUpBoard.append(arr)
+        self.sizeA = len(arr)
+
+        self.solvedBoard = []
+        for line in string2.split('\n'):
+            arr = []
+            for sign in line:
+                arr.append(sign)
+            self.solvedBoard.append(arr)
+        print(self.mixedUpBoard)
+        # print(self.solvedBoard)
+
+    def find_solution(self):
+        output = []
+        # particular solution for size = 2
+        if self.size == 2:
+            output.append()
+            # find something
+
+        return output
+
+    def find_first_line(self):
+        output = []
+        return 0
+
+    def left(self, n):
+        self.mixedUpBoard[n] = self.mixedUpBoard[n][1:] + self.mixedUpBoard[n][:1]
+        self.solution.append("L{}".format(n))
+
+    def right(self, n):
+        self.mixedUpBoard[n] = self.mixedUpBoard[n][-1:] + self.mixedUpBoard[n][:-1]
+        self.solution.append("R{}".format(n))
+
+
+a = LoopOver(input_A, input_B)
+a.right(0)
+print(a.solution)
+print(a.mixedUpBoard)
+
+
+'''
 def right(n):
     m[n][0], m[n][1], m[n][2] = m[n][2], m[n][0], m[n][1]
 
@@ -30,3 +74,4 @@ def output_m(mass):
         print()
         for j in range(3):
             print(mass[i][j], end=' ')
+'''
