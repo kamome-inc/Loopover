736e520c36b7f559cadf370653cb9bbc0dbefbde
Chayka <58815424+kamome-inc@users.noreply.github.com>
2020-02-04 14:16:43 +0300

    add "find first line" method :alarm_clock:
    Дописан метод подбора первой строки. дописаны вспомогательные методы поиска расстояния и координат элемента

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-input_A = "ACS\nBDS\nDCS"
-input_B = "AB\nCD"
+input_A = "URHTP\nWOQKS\nFCLYG\nBDIXJ\nVNEAM"
+input_B = "ABCDE\nFGHIJ\nKLMNO\nPQRST\nUVWXY"
 
 
 class LoopOver:
@@ -9,34 +9,22 @@ class LoopOver:
         # In variable size are exist size of matrix
         self.solution = []
         self.mixedUpBoard = []
+        self.solvedBoard = []
         for line in string1.split('\n'):
             arr = []
             for sign in line:
                 arr.append(sign)
             self.mixedUpBoard.append(arr)
-        self.sizeA = len(arr)
+        self.length = len(arr)
+        self.height = len(self.mixedUpBoard)
 
-        self.solvedBoard = []
         for line in string2.split('\n'):
             arr = []
             for sign in line:
                 arr.append(sign)
             self.solvedBoard.append(arr)
-        print(self.mixedUpBoard)
+        # print(self.mixedUpBoard)
         # print(self.solvedBoard)
-
-    def find_solution(self):
-        output = []
-        # particular solution for size = 2
-        if self.size == 2:
-            output.append()
-            # find something
-
-        return output
-
-    def find_first_line(self):
-        output = []
-        return 0
 
     def left(self, n):
         self.mixedUpBoard[n] = self.mixedUpBoard[n][1:] + self.mixedUpBoard[n][:1]
@@ -46,32 +34,87 @@ class LoopOver:
         self.mixedUpBoard[n] = self.mixedUpBoard[n][-1:] + self.mixedUpBoard[n][:-1]
         self.solution.append("R{}".format(n))
 
+    def up(self, n):
+        arr = []
+        for line in self.mixedUpBoard:
+            arr.append(line[n])
+        arr = arr[1:] + arr[:1]
+        i = 0
+        for line in self.mixedUpBoard:
+            line[n] = arr[i]
+            i += 1
+        self.solution.append("U{}".format(n))
+
+    def down(self, n):
+        arr = []
+        for line in self.mixedUpBoard:
+            arr.append(line[n])
+        arr = arr[-1:] + arr[:-1]
+        i = 0
+        for line in self.mixedUpBoard:
+            line[n] = arr[i]
+            i += 1
+        self.solution.append("D{}".format(n))
+
+    def find_solution(self):
+        # type something
+        return 0
+
+    def find_first_line(self):
+        for sign in self.solvedBoard[0]:
+            move_col, move_row = self.find_the_distance(sign)
+            col, row = self.find_xy(sign, self.mixedUpBoard)
+
+            # moving rows
+            if move_col <= 0:
+                for i in range(abs(move_col)):
+                    self.left(row)
+            if move_col > 0:
+                for i in range(move_col):
+                    self.right(row)
+
+            move_col, move_row = self.find_the_distance(sign)
+            col, row = self.find_xy(sign, self.mixedUpBoard)
+
+            # moving cols
+            if move_row <= 0:
+                for i in range(abs(move_row)):
+                    self.up(col)
+            if move_row > 0:
+                for i in range(move_row):
+                    self.down(col)
+        return 1
+
+    def find_the_distance(self, sign):
+        mixed_x, mixed_y = self.find_xy(sign, self.mixedUpBoard)
+        solved_x, solved_y = self.find_xy(sign, self.solvedBoard)
+        distance_x = solved_x - mixed_x
+        distance_y = solved_y - mixed_y
+        return distance_x, distance_y
+
+    @staticmethod
+    def find_xy(sign, matrix):
+        # x as column, y as line
+        for y in range(len(matrix)):
+            for x in range(len(matrix[y])):
+                if sign == matrix[y][x]:
+                    return x, y
+        raise ValueError("Find_xy: '{}' not found!".format(sign))
+
+    def output(self):
+        print("Mixed:")
+        print("l = " + str(self.length) + " h = " + str(self.height))
+        for line in self.mixedUpBoard:
+            print(line)
+        print("Solved:")
+        for line in self.solvedBoard:
+            print(line)
+
 
 a = LoopOver(input_A, input_B)
-a.right(0)
+a.output()
+print(a.find_first_line())
+a.output()
 print(a.solution)
-print(a.mixedUpBoard)
 
 
-'''
-def right(n):
-    m[n][0], m[n][1], m[n][2] = m[n][2], m[n][0], m[n][1]
-
-
-def left(n):
-    m[n][0], m[n][1], m[n][2] = m[n][1], m[n][2], m[n][0]
-
-
-def up(n):
-    m[0][n], m[1][n], m[2][n] = m[1][n], m[2][n], m[0][n]
-
-
-def down(n):
-    m[0][n], m[1][n], m[2][n] = m[2][n], m[0][n], m[1][n]
-
-def output_m(mass):
-    for i in range(3):
-        print()
-        for j in range(3):
-            print(mass[i][j], end=' ')
-'''
